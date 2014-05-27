cc629d557317a6d739b2ae02effb8f38327d07b3
sqrt <sqrt@entless.org>
2014-05-27 03:28:15 +0200

    fix

--- simpleDuplex.js
+++ simpleDuplex.js
@@ -1,3 +1,21 @@
+/**
+ * lzma-native - Node.js bindings for liblzma
+ * Copyright (C) 2014 Hauke Henningsen <sqrt@entless.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **/
+
 (function() {
 'use strict';
 
@@ -8,26 +26,26 @@ var _ = require('underscore');
 function SimpleDuplex(options, readable, writable) {
 	SimpleDuplex.super_.call(this, options);
 	
-	this.readable = readable;
-	this.writable = writable;
+	this.source = readable;
+	this.sink   = writable;
 	
-	this.readable.on('end',      _.bind(function()    { this.push(null); }, this));
-	this.readable.on('readable', _.bind(function()    { this.read(0); }, this));
-	this.readable.on('error',    _.bind(function(err) { this.emit('error', err); }, this));
-	this.writable.on('error',    _.bind(function(err) { this.emit('error', err); }, this));
-	this.         on('finish',   _.bind(function()    { this.writable.end(); }, this));
+	this.source.on('end',      _.bind(function()    { this.push(null); }, this));
+	this.source.on('readable', _.bind(function()    { this.read(0); }, this));
+	this.source.on('error',    _.bind(function(err) { this.emit('error', err); }, this));
+	this.sink  .on('error',    _.bind(function(err) { this.emit('error', err); }, this));
+	this.         on('finish',   _.bind(function()  { this.sink.end(); }, this));
 }
 
 util.inherits(SimpleDuplex, stream.Duplex);
 
 SimpleDuplex.prototype._read = function(size) {
-	var chunk = this.readable.read(size);
+	var chunk = this.source.read(size);
 	this.push(chunk === null ? '' : chunk);
 	return chunk;
 };
 
 SimpleDuplex.prototype._write = function(chunk, encoding, callback) {
-	return this.writable.write(chunk, encoding, callback);
+	return this.sink.write(chunk, encoding, callback);
 };
 
 exports.SimpleDuplex = SimpleDuplex;
