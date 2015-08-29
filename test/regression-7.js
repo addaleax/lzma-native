7fe42cef1bc534c9575d876a03f78f592f7885e5
Hauke Henningsen <sqrt@entless.org>
2015-08-29 03:01:40 +0200

    Add regression test for #7
    Also, perform some round-trip tests with random test data
    and with Hamlet’s text.

---
+++ test/regression-7.js
@@ -0,0 +1,32 @@
+/**
+ * lzma-native - Node.js bindings for liblzma
+ * Copyright (C) 2014-2015 Hauke Henningsen <sqrt@entless.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **/
+
+'use strict';
+
+var lzma = require('../');
+var fs = require('fs');
+var bl = require('bl');
+
+describe('regression-#7', function() {
+	it('should perform correctly', function(done) {
+		var input = fs.createReadStream('test/random-large');
+		var compressor = lzma.createCompressor({synchronous:true});
+		
+		input.pipe(compressor).pipe(bl());
+	});
+});
