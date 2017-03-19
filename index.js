0988b8d3605a87291bfdd15774d7827f6f6d2dca
Anna Henningsen <anna@addaleax.net>
2017-03-19 18:01:36 +0100

    lib: refactor js-facing Stream into class

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 'use strict';
 
 var stream = require('readable-stream');
-var util = require('util');
 var extend = require('util-extend');
 var assert = require('assert');
 var fs = require('fs');
@@ -23,182 +22,187 @@ Stream.curAsyncStreamsCount = 0;
 
 Stream.prototype.getStream = function(options) {
   options = options || {};
-  
-  var Ret = function(nativeStream) {
-    Ret.super_.call(this, options);
-    var self = this;
-    
-    self.nativeStream = nativeStream;
-    self.synchronous = (options.synchronous || !native.asyncCodeAvailable) ? true : false;
-    self.chunkCallbacks = [];
-    
-    self.totalIn_ = 0;
-    self.totalOut_ = 0;
-    
-    self._writingLastChunk = false;
-    self._isFinished = false;
-    
-    self.totalIn  = function() { return self.totalIn_; };
-    self.totalOut = function() { return self.totalOut_; };
-    
-    self.cleanup = function() {
-      if (self.nativeStream)
-        self.nativeStream.resetUnderlying();
-      
-      self.nativeStream = null;
-    };
-    
-    if (!self.synchronous) {
+
+  return new JSLzmaStream(this, options);
+};
+
+class JSLzmaStream extends stream.Transform {
+  constructor(nativeStream, options) {
+    super(options);
+
+    this.nativeStream = nativeStream;
+    this.synchronous = (options.synchronous || !native.asyncCodeAvailable) ? true : false;
+    this.chunkCallbacks = [];
+
+    this.totalIn_ = 0;
+    this.totalOut_ = 0;
+
+    this._writingLastChunk = false;
+    this._isFinished = false;
+
+    if (!this.synchronous) {
       Stream.curAsyncStreamsCount++;
-      
-      var oldCleanup = self.cleanup;
+
+      var oldCleanup = this.cleanup;
       var countedCleanup = false;
-      self.cleanup = function() {
+      this.cleanup = () => {
         if (countedCleanup === false) {
           Stream.curAsyncStreamsCount--;
           countedCleanup = true;
         }
-        oldCleanup();
+        oldCleanup.call(this);
       };
     }
-    
+
     // always clean up in case of error
-    self.once('error-cleanup', self.cleanup);
-    
-    self.nativeStream.bufferHandler = function(buf, processedChunks, err, totalIn, totalOut) {
+    this.once('error-cleanup', this.cleanup);
+
+    this.nativeStream.bufferHandler = (buf, processedChunks, err, totalIn, totalOut) => {
       if (totalIn !== null) {
-        self.totalIn_  = totalIn;
-        self.totalOut_ = totalOut;
+        this.totalIn_  = totalIn;
+        this.totalOut_ = totalOut;
       }
-      
-      setImmediate(function() {
+
+      setImmediate(() => {
         if (err) {
-          self.push(null);
-          self.emit('error-cleanup', err);
-          self.emit('error', err);
+          this.push(null);
+          this.emit('error-cleanup', err);
+          this.emit('error', err);
         }
-        
+
         if (totalIn !== null) {
-          self.emit('progress', {
-            totalIn: self.totalIn_,
-            totalOut: self.totalOut_
+          this.emit('progress', {
+            totalIn: this.totalIn_,
+            totalOut: this.totalOut_
           });
         }
-        
+
         if (typeof processedChunks === 'number') {
-          assert.ok(processedChunks <= self.chunkCallbacks.length);
-          
-          var chunkCallbacks = self.chunkCallbacks.splice(0, processedChunks);
-          
+          assert.ok(processedChunks <= this.chunkCallbacks.length);
+
+          var chunkCallbacks = this.chunkCallbacks.splice(0, processedChunks);
+
           while (chunkCallbacks.length > 0)
-            chunkCallbacks.shift().apply(self);
+            chunkCallbacks.shift().call(this);
         } else if (buf === null) {
-          if (self._writingLastChunk) {
-            self.push(null);
+          if (this._writingLastChunk) {
+            this.push(null);
           } else {
             // There may be additional members in the file.
             // Reset and set _isFinished to tell `_flush()` that nothing
             // needs to be done.
-            self._isFinished = true;
-            
-            if (self.nativeStream && self.nativeStream._restart) {
-              self.nativeStream._restart();
+            this._isFinished = true;
+
+            if (this.nativeStream && this.nativeStream._restart) {
+              this.nativeStream._restart();
             } else {
-              self.push(null);
+              this.push(null);
             }
           }
         } else {
-          self.push(buf);
+          this.push(buf);
         }
       });
     };
-    
-    // add all methods from the native Stream
-    Object.keys(native.Stream.prototype).forEach(function(key) {
-      self[key] = function() { return self.nativeStream[key].apply(self.nativeStream, arguments); };
-    });
-    
-    Object.defineProperty(self, 'bufsize', {
-      get: function() {
-        return self.setBufsize(null);
-      },
-      set: function(n) {
-        if (typeof n !== 'number' || n <= 0) {
-          throw new TypeError('bufsize must be a positive number');
-        }
-        
-        return self.setBufsize(parseInt(n));
-      }
-    });
-    
+
     if (typeof options.bufsize !== 'undefined') {
-      return self.bufsize = options.bufsize;
+      this.bufsize = options.bufsize;
     }
-  };
-  
-  util.inherits(Ret, stream.Transform);
-  
-  Ret.prototype._transform = function(chunk, encoding, callback) {
+  }
+
+  get bufsize() {
+    return this.setBufsize(null);
+  }
+
+  set bufsize(n) {
+    if (typeof n !== 'number' || n <= 0) {
+      throw new TypeError('bufsize must be a positive number');
+    }
+
+    return this.setBufsize(n);
+  }
+
+  totalIn() {
+    return this.totalIn_;
+  }
+
+  totalOut() {
+    return this.totalOut_;
+  }
+
+  cleanup() {
+    if (this.nativeStream) {
+      this.nativeStream.resetUnderlying();
+    }
+
+    this.nativeStream = null;
+  }
+
+  _transform(chunk, encoding, callback) {
     // Split the chunk at 'YZ'. This is used to have a clean boundary at the
     // end of each `.xz` file stream.
     var possibleEndIndex = bufferIndexOfYZ(chunk);
     if (possibleEndIndex !== -1) {
       possibleEndIndex += 2;
       if (possibleEndIndex !== chunk.length) {
-        this._transform(chunk.slice(0, possibleEndIndex), encoding, function() {
+        this._transform(chunk.slice(0, possibleEndIndex), encoding, () => {
           this._transform(chunk.slice(possibleEndIndex), encoding, callback);
         });
-        
+
         return;
       }
     }
-    
+
     if (this._isFinished && chunk) {
       chunk = skipLeadingZeroes(chunk);
-      
+
       if (chunk.length > 0) {
         // Real data from a second stream member in the file!
         this._isFinished = false;
       }
     }
-    
+
     if (chunk && chunk.length === 0) {
       return callback();
     }
-    
+
     this.chunkCallbacks.push(callback);
-      
+
     try {
       this.nativeStream.code(chunk, !this.synchronous);
     } catch (e) {
       this.emit('error-cleanup', e);
       this.emit('error', e);
     }
-  };
-  
-  Ret.prototype._writev = function(chunks, callback) {
-    chunks = chunks.map(function (chunk) { return chunk.chunk; });
-    this._write(Buffer.concat(chunks), null, callback);
-  };
+  }
 
-  Ret.prototype._flush = function(callback) {
+  _writev(chunks, callback) {
+    chunks = chunks.map(chunk => chunk.chunk);
+    this._write(Buffer.concat(chunks), null, callback);
+  }
+
+  _flush(callback) {
     this._writingLastChunk = true;
-    var cleanup = this.cleanup;
-    
+
     if (this._isFinished) {
-      cleanup();
+      this.cleanup();
       callback(null);
       return;
     }
 
     this._transform(null, null, function() {
-      cleanup();
+      this.cleanup();
       callback.apply(this, arguments);
     });
+  }
+}
+
+// add all methods from the native Stream
+Object.keys(native.Stream.prototype).forEach(function(key) {
+  JSLzmaStream.prototype[key] = function() {
+    return this.nativeStream[key].apply(this.nativeStream, arguments);
   };
-  
-  return new Ret(this);
-};
+});
 
 Stream.prototype.rawEncoder = function(options) {
   return this.rawEncoder_(options.filters || []);
@@ -269,19 +273,19 @@ exports.createStream = function(coder, options) {
     options = coder;
     coder = null;
   }
-  
+
   if (parseInt(options) === parseInt(options))
     options = {preset: parseInt(options)};
-  
+
   coder = coder || 'easyEncoder';
   options = options || {};
-  
+
   var stream = new Stream();
   stream[coder](options);
-  
+
   if (options.memlimit)
     stream.memlimitSet(options.memlimit);
-  
+
   return stream.getStream(options);
 };
 
@@ -298,20 +302,20 @@ exports.crc32 = function(input, encoding, presetCRC32) {
     presetCRC32 = encoding;
     encoding = null;
   }
-  
-  if (typeof input === 'string') 
+
+  if (typeof input === 'string')
     input = new Buffer(input, encoding);
-  
+
   return exports.crc32_(input, presetCRC32 || 0);
 };
 
 /* compatibility: node-xz (https://github.com/robey/node-xz) */
 exports.Compressor = function(preset, options) {
   options = extend({}, options);
-  
+
   if (preset)
     options.preset = preset;
-  
+
   return createStream('easyEncoder', options);
 };
 
@@ -323,21 +327,20 @@ exports.Decompressor = function(options) {
 function singleStringCoding(stream, string, on_finish, on_progress) {
   on_progress = on_progress || function() {};
   on_finish = on_finish || function() {};
-  
+
   // possibly our input is an array of byte integers
   // or a typed array
   if (!Buffer.isBuffer(string))
     string = new Buffer(string);
-  
+
   var deferred = {}, failed = false;
-  
+
   stream.once('error', function(err) {
     failed = true;
     on_finish(null, err);
   });
 
   // emulate Promise.defer()
-  var deferred = {};
   deferred.promise = new Promise(function(resolve, reject) {
     deferred.resolve = resolve;
     deferred.reject = reject;
@@ -346,33 +349,33 @@ function singleStringCoding(stream, string, on_finish, on_progress) {
   // Since using the Promise API is optional, generating unhandled
   // rejections is not okay.
   deferred.promise.catch(noop);
-    
+
   stream.once('error', function(e) {
     deferred.reject(e);
   });
-  
+
   var buffers = [];
 
   stream.on('data', function(b) {
     buffers.push(b);
   });
-  
+
   stream.once('end', function() {
     var result = Buffer.concat(buffers);
-    
+
     if (!failed) {
       on_progress(1.0);
       on_finish(result);
     }
-    
+
     if (deferred)
       deferred.resolve(result);
   });
 
   on_progress(0.0);
-  
+
   stream.end(string);
-  
+
   if (deferred)
     return deferred.promise;
 }
@@ -386,12 +389,12 @@ exports.LZMA = function() {
         opt.preset = parseInt(mode);
 
       var stream = createStream('aloneEncoder', opt);
-      
+
       return singleStringCoding(stream, string, on_finish, on_progress);
     },
     decompress: function(byte_array, on_finish, on_progress) {
       var stream = createStream('autoDecoder');
-      
+
       return singleStringCoding(stream, byte_array, on_finish, on_progress);
     },
     // dummy, we don’t use web workers
@@ -404,7 +407,7 @@ exports.compress = function(string, opt, on_finish) {
     on_finish = opt;
     opt = {};
   }
-  
+
   var stream = createStream('easyEncoder', opt);
   return singleStringCoding(stream, string, on_finish);
 };
@@ -414,7 +417,7 @@ exports.decompress = function(string, opt, on_finish) {
     on_finish = opt;
     opt = {};
   }
-  
+
   var stream = createStream('autoDecoder', opt);
   return singleStringCoding(stream, string, on_finish);
 };
@@ -433,64 +436,64 @@ exports.parseFileIndex = function(options, callback) {
   if (typeof options !== 'object') {
     throw new TypeError('parseFileIndex needs an options object');
   }
-  
+
   var p = new native.IndexParser();
-  
+
   if (typeof options.fileSize !== 'number') {
     throw new TypeError('parseFileeIndex needs options.fileSize');
   }
-  
+
   if (typeof options.read !== 'function') {
     throw new TypeError('parseFileIndex needs a read callback');
   }
-  
+
   p.init(options.fileSize, options.memlimit || 0);
   p.read_cb = function(count, offset) {
     var inSameTick = true;
     var bytesRead = count;
-    
+
     options.read(count, offset, function(err, buffer) {
       if (Buffer.isBuffer(err)) {
         buffer = err;
         err = null;
       }
-      
+
       if (err) {
         if (typeof callback === 'undefined') {
           throw err;
         }
-        
+
         return callback(err, null);
       }
-      
+
       p.feed(buffer);
       bytesRead = buffer.length;
-      
+
       if (inSameTick) {
         // The call to parse() is still on the call stack and will continue
         // seamlessly.
         return;
       }
-      
+
       // Kick off parsing again.
       var info;
-      
+
       try {
         info = p.parse();
       } catch (e) {
         return callback(e, null);
       }
-      
+
       if (info !== true) {
         return callback(null, cleanupIndexInfo(info));
       }
     });
-    
+
     inSameTick = false;
-    
+
     return bytesRead;
   };
-  
+
   var info;
   try {
     info = p.parse();
@@ -499,16 +502,16 @@ exports.parseFileIndex = function(options, callback) {
       callback(e, null);
       return;
     }
-    
+
     throw e;
   }
-  
+
   if (info !== true) {
     info = cleanupIndexInfo(info);
     if (typeof callback !== 'undefined' && info !== true) {
       callback(null, info);
     }
-    
+
     return info;
   }
 };
@@ -518,21 +521,21 @@ exports.parseFileIndexFD = function(fd, callback) {
     if (err) {
       return callback(err, null);
     }
-    
+
     exports.parseFileIndex({
       fileSize: stats.size,
       read: function(count, offset, cb) {
         var buffer = new Buffer(count);
-        
+
         fs.read(fd, buffer, 0, count, offset, function(err, bytesRead, buffer) {
           if (err) {
             return cb(err, null);
           }
-          
+
           if (bytesRead !== count) {
             return cb(new Error('Truncated file!'), null);
           }
-          
+
           cb(null, buffer);
         });
       }
@@ -542,13 +545,13 @@ exports.parseFileIndexFD = function(fd, callback) {
 
 function cleanupIndexInfo(info) {
   var checkFlags = info.checks;
-  
+
   info.checks = [];
   for (var i = 0; i < exports.CHECK_ID_MAX; i++) {
     if (checkFlags & (1 << i))
       info.checks.push(i);
   }
-  
+
   return info;
 }
 
@@ -558,7 +561,7 @@ function skipLeadingZeroes(buffer) {
     if (buffer[i] !== 0x00)
       break;
   }
-  
+
   return buffer.slice(i);
 }
 
@@ -566,18 +569,18 @@ function bufferIndexOfYZ(chunk) {
   if (!chunk) {
     return -1;
   }
-  
+
   if (chunk.indexOf) {
     return chunk.indexOf('YZ');
   }
-  
+
   var i;
   for (i = 0; i < chunk.length - 1; i++) {
     if (chunk[i] === 0x59 && chunk[i+1] === 0x5a) {
       return i;
     }
   }
-  
+
   return -1;
 }
 
