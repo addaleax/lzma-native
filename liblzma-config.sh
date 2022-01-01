4e04ae3253afcc96564d014509af6761bfc4cec3
tylinux <tylinux@xnure.com>
2022-01-02 01:24:09 +0800

    build: fix build on Apple Silicon

--- liblzma-config.sh
+++ liblzma-config.sh
@@ -10,6 +10,14 @@ cd "$TARGET_DIR"
 tar xvjf "$SRC_TARBALL" >node_liblzma_config.log 2>&1 
 
 export CFLAGS="-fPIC $CFLAGS"
+
+# Fix build on Apple Silicon
+if [[ ("$OSTYPE" == "darwin"*) && ($(uname -m) == "arm64") ]]; then
+	XZ_SRC_DIR=$(ls | grep xz-*)
+	sed -i.bak 's/\tnone)/\tarm64-*)\n\t\tbasic_machine=$(echo $basic_machine | sed "s\/arm64\/aarch64\/")\n\t\t;;\n\t\tnone)/g' $XZ_SRC_DIR/build-aux/config.sub
+	rm $XZ_SRC_DIR/build-aux/config.sub.bak
+fi
+
 sh xz-*/configure --enable-static --disable-shared --disable-scripts --disable-lzmainfo \
 	--disable-lzma-links --disable-lzmadec --disable-xzdec --disable-xz --disable-rpath \
 	--prefix="$TARGET_DIR/build" CFLAGS="$CFLAGS" >>node_liblzma_config.log 2>&1 
