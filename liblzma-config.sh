13c11da617154cfe572e981f7bfd2bf160b25d42
Anton Matosov <amatosov@roblox.com>
2022-04-29 14:12:32 -0700

    quotes

--- liblzma-config.sh
+++ liblzma-config.sh
@@ -14,7 +14,7 @@ tar xvjf "$SRC_TARBALL" >node_liblzma_config.log 2>&1
 export CFLAGS="-fPIC $CFLAGS"
 
 # Fix build on Apple Silicon
-if [[ $(uname) = "Darwin" && ( $(uname -m) = "arm64" || "$PREBUILD_ARCH" = "arm64" ) ]]; then
+if [[ "$(uname)" = "Darwin" && ( "$(uname -m)" = "arm64" || "$PREBUILD_ARCH" = "arm64" ) ]]; then
     XZ_SRC_DIR=$(ls | grep xz-*)
     sed -i '' 's/\tnone)/\tarm64-*)\n\t\tbasic_machine=$(echo $basic_machine | sed "s\/arm64\/aarch64\/")\n\t\t;;\n\t\tnone)/g' $XZ_SRC_DIR/build-aux/config.sub
 fi
