52b41904769f33cb2795a2834ad771ded902faf3
Anton Matosov <amatosov@roblox.com>
2022-04-29 13:43:15 -0700

    try checking PREBUILD_ARCH

--- liblzma-config.sh
+++ liblzma-config.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
+set -x
+
+env
 
 SRC_TARBALL="$2"
 TARGET_DIR="$1/liblzma"
@@ -12,7 +15,7 @@ tar xvjf "$SRC_TARBALL" >node_liblzma_config.log 2>&1
 export CFLAGS="-fPIC $CFLAGS"
 
 # Fix build on Apple Silicon
-if [ $(uname) = "Darwin" -a $(uname -m) = "arm64" ]; then
+if [ $(uname) = "Darwin" -a [$(uname -m) = "arm64" -o "$PREBUILD_ARCH" = "arm64" ] ]; then
     XZ_SRC_DIR=$(ls | grep xz-*)
     sed -i '' 's/\tnone)/\tarm64-*)\n\t\tbasic_machine=$(echo $basic_machine | sed "s\/arm64\/aarch64\/")\n\t\t;;\n\t\tnone)/g' $XZ_SRC_DIR/build-aux/config.sub
 fi
